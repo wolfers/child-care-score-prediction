aa5ab70fce48ef6f9c3eebbfb8fe1a795a8821da
Trevor Wolfe <trevo1992@gmail.com>
2018-08-27 16:45:36 -0700

    added functions for official rating predictions and moved other classes to demo

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,11 +1,39 @@
-let get_predict = function(test_type) {
+let fake_predict = function(test_type) {
+    if (test_type == 1) {
+        var url = '/predictfakeERS'
+    }
+    else {
+        var url = '/predictfakeCLASS'
+    }
+    $.ajax({
+        url: url,
+        contentType: "application/json; charset=utf-8",
+        type: 'POST',
+        success: function (data) {
+            display_fake_prediction(data)
+        }
+    });
+};
+
+let display__fake_prediction = function(prediction) {
+    if (prediction.test_type == "ers") {
+        $("img#prediction_ers").attr('src', '/static/images/demo/ers_graph.jpg')
+    }
+    else if (prediction.test_type == "class") {
+        $("img#prediction_class").attr('src', '/static/images/demo/class_graph.jpg')
+    }
+};
+
+let get_graph = function(test_type) {
     if (test_type == 1) {
         var url = '/predictERS'
     }
     else {
         var url = '/predictCLASS'
     }
+    var data = $('form').serializeArray()
     $.ajax({
+        data: JSON.stringify(data),
         url: url,
         contentType: "application/json; charset=utf-8",
         type: 'POST',
@@ -14,6 +42,7 @@ let get_predict = function(test_type) {
         }
     });
 };
+
 let display_prediction = function(prediction) {
     if (prediction.test_type == "ers") {
         $("img#prediction_ers").attr('src', '/static/images/ers_graph.jpg')
