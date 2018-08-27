4bf572a46396fdb1e2339217bde6e387dfd6e7cd
Trevor Wolfe <trevo1992@gmail.com>
2018-08-27 16:44:41 -0700

    added functions for cleaning json form data and creates ers graphs

--- app/app.py
+++ app/app.py
@@ -1,9 +1,11 @@
 from flask import Flask, request, render_template, jsonify
 from sklearn.ensemble import RandomForestRegressor
+import sys
 import matplotlib.pyplot as plt
 import pandas as pd
 import pickle
 import requests
+from DataCleaning import CleanClass, CleanErs
 
 app = Flask(__name__)
 
@@ -13,27 +15,41 @@ def dashboard():
 
 @app.route('/ers')
 def ers():
-    ers_html = create_ers_html()
+    ers_html = create_html('ers')
     return render_template('ers.html', value=ers_html)
 
 @app.route('/class')
 def class_page():
-    return render_template('class.html')
+    class_html = create_html('class')
+    return render_template('class.html', value=class_html)
 
-@app.route('/predictERS', methods=["POST"])
-def predictERS():
+@app.route('/predictfakeERS', methods=["POST"])
+def predictfakeERS():
     df = pd.read_csv('data/fake_ers_X.csv', sep='|')
     df_times = create_times(df)
     predictions = ers_model.predict(df_times)
-    create_graph(df_times['time_delta'].values, predictions, 'static/images/ers_graph.jpg')
+    create_graph(df_times['time_delta'].values, predictions, 'static/images/demo/ers_graph.jpg')
     return jsonify({'test_type': 'ers'})
 
-@app.route('/predictCLASS', methods=["POST"])
-def predictCLASS():
+@app.route('/predictfakeCLASS', methods=["POST"])
+def predictfakeCLASS():
     df = pd.read_csv('data/fake_class_X.csv', sep='|')
     df_times = create_times(df)
     predictions = class_model.predict(df_times)
-    create_graph(df_times['time_delta'].values, predictions, 'static/images/class_graph.jpg')
+    create_graph(df_times['time_delta'].values, predictions, 'static/images/demo/class_graph.jpg')
+    return jsonify({'test_type': 'class'})
+
+@app.route('/predictERS', methods=["POST"])
+def predictERS():
+    df = create_df_from_form(request.json)
+    df_cleaned = ers_transformer(df)
+    df_times = create_times(df_cleaned)
+    predictions = ers_model.predict(df_times)
+    create_graph(df_times['time_delta'].values, predictions, 'static/images/prediction-graphs/ers_graph.jpg')
+    return jsonify({'test_type': 'ers'})
+
+@app.route('/predictCLASS', methods=["post"])
+def predictCLASS():
     return jsonify({'test_type': 'class'})
 
 @app.errorhandler(404)
@@ -55,13 +71,30 @@ def create_graph(times, data, filename):
     plt.ylabel('Average Score')
     plt.savefig(filename)
 
-def create_ers_html():
-    return "<p>help</p>"
+def create_html(test_type):
+    final_html = ''
+    html_string = '<label>{0}</label><input type="text" name="{0}">'
+    if test_type == "ers":
+        transformer = ers_transformer
+    else:
+        transformer = class_transformer
+    for score_col in transformer.score_col_names:
+        final_html += (html_string.format(score_col))
+    return final_html
+
+def create_df_from_form(json_form):
+    to_be_df = []
+    for form_input in json_form:
+        to_be_df.append({form_input['name']: form_input['value']})
+    return pd.DataFrame(to_be_df)
 
 if __name__ == '__main__':
-    #load pickled models for use in prediction
     with open('static/ers_model.pkl', 'rb') as f:
         ers_model = pickle.load(f)
     with open('static/class_model.pkl', 'rb') as f:
         class_model = pickle.load(f)
+    with open('static/ers_trans_model.pkl', 'rb') as f:
+        ers_transformer = pickle.load(f)
+    with open('static/class_trans_model.pkl', 'rb') as f:
+        class_transformer = pickle.load(f)
     app.run(host='0.0.0.0', port=8080, debug=True)
